638aced5493c9e1134f35a58fd2bb835c78a6075
xenoveal <xenoveals@gmail.com>
2022-05-10 12:44:03 +0700

    enemy move

---
+++ Assets/Script/EnemyNav.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyNav : MonoBehaviour
+{
+    [SerializeField] private Transform movePositionTransform;
+    private NavMeshAgent navMeshAgent;
+    private void Awake() {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update() {
+        navMeshAgent.destination = movePositionTransform.position;
+    }
+}
